1e1460277d791061cfe6de6a58c5b3693920450b
devnamdev2003 <devnamdevcse@gmail.com>
2026-04-19 16:37:08 +0530

    update

--- dev.md
+++
@@ -1,8 +0,0 @@
-const mediaList = [
-    {
-        title: "Spring boot jwt",
-        description: "",
-        videoPath: "./video.mp4",
-        audioPath: "./audio.mp4"
-    }
-];
--- vasync/ytd.py
+++ vasync/ytd.py
@@ -2,23 +2,26 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
 import os
 
-url = "https://www.youtube.com/watch?v=q2l91Ffc_8U"
+URLs = [
+""
+]
+count = 1
+for url in URLs:
+    yt = YouTube(url, on_progress_callback=on_progress)
+    print("Title:", yt.title)
 
-yt = YouTube(url, on_progress_callback=on_progress)
-print("Title:", yt.title)
+    # 🎯 Step 1: Get 1080p VIDEO (no audio)
+    video_stream = yt.streams.filter(res="1080p", file_extension="mp4").first()
 
-# 🎯 Step 1: Get 1080p VIDEO (no audio)
-video_stream = yt.streams.filter(res="1080p", file_extension="mp4").first()
+    if not video_stream:
+        print("1080p not available for this video")
+        exit()
 
-if not video_stream:
-    print("1080p not available for this video")
-    exit()
+    # 🎯 Step 2: Get AUDIO
+    audio_stream = yt.streams.filter(only_audio=True).first()
 
-# 🎯 Step 2: Get AUDIO
-audio_stream = yt.streams.filter(only_audio=True).first()
-
-# 🎯 Step 3: Download both
-video_file = video_stream.download(filename="video.mp4")
-audio_file = audio_stream.download(filename="audio.mp4")
-
-print("Downloaded video + audio")
+    # 🎯 Step 3: Download both
+    video_file = video_stream.download(filename="video" + str(count) + ".mp4")
+    audio_file = audio_stream.download(filename="audio" + str(count) + ".mp4")
+    count = count + 1
+    print("Downloaded video + audio")
